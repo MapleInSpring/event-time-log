06f2499ddd5f40ab1706edea80f1288c8f8cfb5e
MapleInSpring <hyf94244280@gmail.com>
2017-08-09 13:58:29 +0800

    Display a list of events
    [#148193149]

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,16 @@ import {Component} from '@angular/core';
 import {onsNotification} from 'angular2-onsenui';
 
 @Component({
-  selector: 'app',
-  template: require('./app.html'),
-  styles: [require('./app.css')]
+    selector: 'app',
+    template: require('./app.html'),
+    styles: [require('./app.css')]
 })
 export class MyApp {
-  constructor() {}
+    events = [
+        {name: "event1", description: "first event"},
+        {name: "event2", description: "second event"}
+    ];
 
-  alert() {
-    onsNotification.alert('This is an Onsen UI alert notification test.');
-  }
+    constructor() {
+    }
 }
